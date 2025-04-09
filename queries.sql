22f959844806e4b7f58fb3afafb022ba351b50ab
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-04-09 14:54:55 +0530

    UC2: create payroll table

--- queries.sql
+++ queries.sql
@@ -5,3 +5,16 @@ create database payroll_service;
 show databases;
 
 use payroll_service;
+
+
+
+
+-- # UC 2 - Ability to create a employee payroll table in the payroll service database
+
+CREATE TABLE employee_payroll (
+    id INT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    salary DOUBLE,
+    start_date DATE,
+    PRIMARY KEY (id)
+);
